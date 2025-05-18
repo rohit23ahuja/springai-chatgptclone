98fe763f68a1529661e2593f58fa0017d44ab78f
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-18 18:56:03 +0530

    spring ai htmx thymeleaf

--- src/main/java/dev/rohitahuja/chatgpt/Application.java
+++ src/main/java/dev/rohitahuja/chatgpt/Application.java
@@ -1,13 +1,16 @@
 package dev.rohitahuja.chatgpt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringaiChatgptcloneApplication {
+public class Application {
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringaiChatgptcloneApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
