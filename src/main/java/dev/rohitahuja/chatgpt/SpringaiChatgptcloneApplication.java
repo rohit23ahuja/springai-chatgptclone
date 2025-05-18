d0718d727e449fb67769b17c3a5a4dd441c8a396
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-18 17:55:22 +0530

    initial commit

---
+++ src/main/java/dev/rohitahuja/chatgpt/SpringaiChatgptcloneApplication.java
@@ -0,0 +1,13 @@
+package dev.rohitahuja.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiChatgptcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiChatgptcloneApplication.class, args);
+	}
+
+}
